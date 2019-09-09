a79986732457c8260de281d5aa8cb3c2b13b6f86
noseparte <noseparte@aliyun.com>
2019-09-09 11:57:09 +0800

    game 测试

---
+++ h5web-game/src/test/java/com/xmbl/h5/web/game/H5WebGameApplicationTest.java
@@ -0,0 +1,38 @@
+package com.xmbl.h5.web.game;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class H5WebGameApplicationTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public H5WebGameApplicationTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( H5WebGameApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
