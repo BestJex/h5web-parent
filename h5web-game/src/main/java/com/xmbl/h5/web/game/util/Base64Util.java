37efd95a120633f9374c55b6b7a38e74f2514346
noseparte <noseparte@aliyun.com>
2019-09-09 11:58:50 +0800

    Base64

---
+++ h5web-game/src/main/java/com/xmbl/h5/web/game/util/Base64Util.java
@@ -0,0 +1,63 @@
+package com.xmbl.h5.web.game.util;
+
+import sun.misc.BASE64Decoder;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Copyright © 2018 noseparte © BeiJing BoLuo Network Technology Co. Ltd.
+ *
+ * @Author Noseparte
+ * @Compile 2018-12-24 -- 18:16
+ * @Version 1.0
+ * @Description
+ */
+public class Base64Util {
+
+    /**
+     * 将 s 进行 BASE64 编码
+     *
+     * @return String
+     */
+    public static String encode(String s) {
+        if (s == null)
+            return null;
+        String res = "";
+        try {
+            res = new sun.misc.BASE64Encoder().encode(s.getBytes("GBK"));
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     * 将 BASE64 编码的字符串 s 进行解码
+     *
+     * @date 2015-3-4 上午09:24:26
+     */
+    public static String decode(String s) {
+        if (s == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            byte[] b = decoder.decodeBuffer(s);
+            return new String(b,"UTF-8");
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    /**
+     *
+     * @return void
+     * @author lifq
+     * @date 2015-3-4 上午09:23:17
+     */
+    public static void main(String[] args) {
+        System.out.println(Base64Util.encode("哈哈"));
+        System.out.println(Base64Util.decode("uf65/g=="));
+
+    }
+
+}
