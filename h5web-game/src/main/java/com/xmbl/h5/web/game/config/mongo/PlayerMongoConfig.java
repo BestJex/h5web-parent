599c56f6aae0775b7ecc6a6cda355b7888ae9dc2
noseparte <noseparte@aliyun.com>
2019-09-09 12:01:31 +0800

    mongoTemplateLogin

---
+++ h5web-game/src/main/java/com/xmbl/h5/web/game/config/mongo/PlayerMongoConfig.java
@@ -0,0 +1,21 @@
+package com.xmbl.h5.web.game.config.mongo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.xmbl.h5.web.common.db.mongo.AbstractMongoConfig;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+@ConfigurationProperties(prefix = "spring.data.mongo.player")
+public class PlayerMongoConfig extends AbstractMongoConfig {
+
+	@Override
+	@Bean(name = "mongoTemplateLogin")
+	public MongoTemplate getMongoTemplate() throws Exception {
+		return new MongoTemplate(mongoDbFactory());
+	}
+}
