59e0589231896f4cdfce8fa0dd74579c384cad36
noseparte <noseparte@aliyun.com>
2019-09-09 12:03:22 +0800

    Game服 全局系统参数

---
+++ h5web-game/src/main/java/com/xmbl/h5/web/game/consts/GameConst.java
@@ -0,0 +1,25 @@
+package com.xmbl.h5.web.game.consts;
+
+public class GameConst {
+	
+	public static final int share_second_return_energy = 15;
+	
+	public static final int tree_stage_result_fail = 0;
+	public static final int tree_stage_result_success = 1;
+	
+	public static final int tree_node_status_unpass = 0;
+	public static final int tree_node_status_pass = 1;
+	public static final int tree_node_status_skip = 2;
+	
+	public static final int tree_stage_status_noinfo = 0;
+	public static final int tree_stage_status_unpass = 1;
+	public static final int tree_stage_status_pass = 2;
+	public static final int tree_stage_status_skip = 3;
+	
+	public static final int player_nodeInfo_status_unFinish = 0;
+	public static final int player_nodeInfo_status_finish = 1;
+	public static final int player_nodeInfo_status_skip = 2;
+
+	public static final int player_treeInfo_maxEnergy = 25;
+
+}
---
+++ h5web-game/src/main/java/com/xmbl/h5/web/game/consts/MongoDBConst.java
@@ -0,0 +1,10 @@
+package com.xmbl.h5.web.game.consts;
+
+public class MongoDBConst {
+	
+	public static final String stageTreeShop = "stageTreeShop";
+	public static final String stageTreeRes = "stageTreeRes";
+	
+	public static final String firstPassInfo = "firstPassInfo";
+
+}
