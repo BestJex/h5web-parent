3835cce0a075bc88d124010fb0e09eb83b36a2ac
noseparte <noseparte@aliyun.com>
2019-09-09 11:24:09 +0800

    rabbitmq 常量

---
+++ h5web-common/src/main/java/com/xmbl/h5/web/common/consts/RabbitConst.java
@@ -0,0 +1,10 @@
+package com.xmbl.h5.web.common.consts;
+
+public class RabbitConst {
+	public static final String h5_web_rank_notice_queue = "h5.web.rank.notice.queue";
+	
+	public static final String h5_web_rank_query_rpc_queue = "h5.web.rank.query.rpc.queue";
+	public static final String h5_web_rank_query_rpc_exchange = "h5.web.rank.query.rpc.exchange";
+	public static final String h5_web_rank_query_rpc_route_key = "h5.web.rank.query.rpc.route.key";
+
+}
