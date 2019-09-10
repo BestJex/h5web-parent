4ed67f1a9fcf3ce32a1f0a43d4d5790d9fd03f35
noseparte <noseparte@aliyun.com>
2019-09-10 12:24:34 +0800

    Rank 全局系统参数

---
+++ h5web-rank/src/main/java/com/xmbl/h5/web/rank/consts/RankConsts.java
@@ -0,0 +1,15 @@
+package com.xmbl.h5.web.rank.consts;
+
+public class RankConsts {
+	public static final int tree_rank_size = Integer.MAX_VALUE;
+	public static final int node_rank_size = Integer.MAX_VALUE;
+	public static final int stage_rank_size = 100;
+	
+	public static final String tree_rank_mongo_cname_prefix = "tree_rank_";
+	public static final String node_rank_mongo_cname_prefix = "node_rank_";
+	public static final String stage_rank_mongo_cname_prefix = "stage_rank_";
+	
+	public static final String tree_rank_collection_list = "tree_rank_collection_list";
+	public static final String node_rank_collection_list = "node_rank_collection_list";
+	public static final String stage_rank_collection_list = "stage_rank_collection_list";
+}
